e84a4bfaa0d88a53a1f4856a3dfb6627d04c8c11
Suresh GK <65127022+Suresh-GK1@users.noreply.github.com>
2023-01-30 18:38:52 +0530

    Create Produce a list of costly bookings, using a subquery.sql

---
+++ solutions/Produce a list of costly bookings, using a subquery.sql
@@ -0,0 +1,7 @@
+/*Produce a list of costly bookings, using a subquery
+
+Question
+
+The Produce a list of costly bookings exercise contained some messy logic: we had to calculate the booking cost in both the WHERE clause and the CASE statement. Try to simplify this calculation using subqueries. For reference, the question was:
+How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30? Remember that guests have different costs to members (the listed costs are per half-hour 'slot'), and the guest user is always ID 0. Include in your output the name of the facility, 
+the name of the member formatted as a single column, and the cost. Order by descending cost.*/
